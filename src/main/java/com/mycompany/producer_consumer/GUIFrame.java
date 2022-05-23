c097b62be096d4dd0fdc62fcdc8daf4a79b60034
Luis Mario <luis.mario.trujillo@live.com.mx>
2022-05-23 11:29:57 -0500

    Small changes test, solved conflicts and rebase

--- src/main/java/com/mycompany/producer_consumer/GUIFrame.java
+++ src/main/java/com/mycompany/producer_consumer/GUIFrame.java
@@ -1,7 +1,5 @@
 package com.mycompany.producer_consumer;
 
-import javax.swing.JOptionPane;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -38,14 +36,14 @@ public class GUIFrame extends javax.swing.JFrame {
         jTextField2 = new javax.swing.JTextField();
         buffer_size_input = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
-        rangoDeValoresAlto = new javax.swing.JSpinner();
+        jTextField4 = new javax.swing.JTextField();
+        jSpinner3 = new javax.swing.JSpinner();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         Productores_cantidad = new javax.swing.JSpinner();
         Consumidores_cantidad = new javax.swing.JSpinner();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        rangoDeValoresBajo = new javax.swing.JSpinner();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
@@ -65,8 +63,6 @@ public class GUIFrame extends javax.swing.JFrame {
 
         jLabel6.setText("Rango de Valores (n, m)");
 
-        rangoDeValoresAlto.setModel(new javax.swing.SpinnerNumberModel(0, 0, 9, 1));
-
         jLabel1.setText("Productores");
 
         jLabel2.setText("Consumidores");
@@ -76,8 +72,6 @@ public class GUIFrame extends javax.swing.JFrame {
         jLabel3.setText("Tamaño del Buffer");
 
         jLabel4.setText("Cantidad");
-
-        rangoDeValoresBajo.setModel(new javax.swing.SpinnerNumberModel(0, 0, 9, 1));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -93,18 +87,17 @@ public class GUIFrame extends javax.swing.JFrame {
                     .addComponent(jLabel6))
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(Consumidores_cantidad)
-                        .addComponent(buffer_size_input)
-                        .addComponent(Productores_cantidad)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE))
-                    .addComponent(rangoDeValoresBajo, javax.swing.GroupLayout.DEFAULT_SIZE, 92, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(Consumidores_cantidad)
+                    .addComponent(buffer_size_input)
+                    .addComponent(Productores_cantidad)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jTextField1)
                     .addComponent(jTextField2)
-                    .addComponent(rangoDeValoresAlto, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jSpinner3, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(52, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -132,8 +125,8 @@ public class GUIFrame extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(rangoDeValoresAlto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(rangoDeValoresBajo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jSpinner3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(48, Short.MAX_VALUE))
         );
 
@@ -278,16 +271,6 @@ public class GUIFrame extends javax.swing.JFrame {
         // que hacer cuando se pique start!
         System.out.println("BUTTON-----START------CLICKED!");
         
-        
-        // Guardar los valores de rangos de operaciones scheme
-        int rangoValoresBajo = (Integer) rangoDeValoresBajo.getValue();
-        int rangoValoresAlto = (Integer) rangoDeValoresAlto.getValue();
-        
-        if (rangoValoresBajo > rangoValoresAlto) {
-            JOptionPane.showMessageDialog(null, "El rango es incorrecto!");
-        }
-
-        
         //implementar logica de buffer size
         int buffer_size = Integer.parseInt( buffer_size_input.getText());
         Buffer buffer = new Buffer(buffer_size);
@@ -383,13 +366,13 @@ public class GUIFrame extends javax.swing.JFrame {
     private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JSpinner jSpinner3;
     private javax.swing.JSpinner jSpinner4;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
-    private javax.swing.JSpinner rangoDeValoresAlto;
-    private javax.swing.JSpinner rangoDeValoresBajo;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
